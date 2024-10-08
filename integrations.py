ffa0172777f4c79a87894383e6228ca0d9e91c7a
SamuelFSNunes <samucasantana2003@gmail.com>
2024-10-08 17:18:56 -0300

    ref: add new functions to integrations

--- integrations.py
+++ integrations.py
@@ -1,51 +1,17 @@
-from demo import select_bq
+from demo import select_bq, get_schemas
 from demoAI import generate, get_query_in_response, create_sqlx
 from dataformTest import upload_sqlx_file, execute_job
 
-query = """SELECT * FROM `squadcalouros.dataform.eventos`"""
+table_observed = "squadcalouros.dataform.eventos"
+
+query = f"""SELECT * FROM `{table_observed}`"""
 select_return = select_bq(query)
 for row in select_return:
     select_return = row["data"]
 
-schema = [
-  {
-    "name": "subscription_name",
-    "mode": "",
-    "type": "STRING",
-    "description": "",
-    "fields": []
-  },
-  {
-    "name": "message_id",
-    "mode": "",
-    "type": "STRING",
-    "description": "",
-    "fields": []
-  },
-  {
-    "name": "publish_time",
-    "mode": "",
-    "type": "TIMESTAMP",
-    "description": "",
-    "fields": []
-  },
-  {
-    "name": "data",
-    "mode": "",
-    "type": "JSON",
-    "description": "",
-    "fields": []
-  },
-  {
-    "name": "attributes",
-    "mode": "",
-    "type": "STRING",
-    "description": "",
-    "fields": []
-  }
-]
+schema = get_schemas(table_path=table_observed)
 
-message = f"""Tenho uma tabela no Big Query chamada "squadcalouros.dataform.eventos" que possui o seguinte esquema {schema}.
+message = f"""Tenho uma tabela no Big Query chamada {table_observed} que possui o seguinte esquema {schema}.
 Meu objetivo é criar uma VIEW a partir de um SQLX apenas com os valores que estão na coluna "data" e quero que esses dados estejam no mesmo tipos que estão no JSON,  como ela está em um formato JSON será necessário usar a função JSON_VALUE. {select_return}, Me retorne apenas a query SQLX"""
 
 response_chat = generate(message)
@@ -54,11 +20,14 @@ querysql = get_query_in_response(response_chat.text)
 
 create_sqlx(querysql, "eventos_data")
 
-project_id = "squadcalouros"
+project_id = "squadcalouros" # ID do projeto
+dataset_id = "dataform" # ID do dataset
 repository_id = "teste"  # Nome simples do repositório
 workspace = "testDataform"  # A branch onde você quer fazer o upload
+file_name = "eventos_view" # Nome do arquivo
 file_path_local = "eventos_data.sqlx"  # Caminho do arquivo local
-file_path_repo = "eventos_view.sqlx"  # Nome do arquivo dentro do repositório
+file_path_repo = file_name + ".sqlx"  # Nome do arquivo dentro do repositório
+
 
 upload_sqlx_file(project_id, repository_id, workspace, file_path_local, file_path_repo)
-execute_job(project_id, repository_id, workspace, "dataform", "eventos_view")
+execute_job(project_id, repository_id, workspace, dataset_id, file_name)
